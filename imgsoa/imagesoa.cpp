500d0fc58f300b654e4fd2447fe5df660541c769
100495914 <100495914@alumnos.uc3m.es>
2024-11-05 05:33:55 +0100

    rebase main

--- imgsoa/imagesoa.cpp
+++ imgsoa/imagesoa.cpp
@@ -1,3 +1,41 @@
 //
 // Created by diego on 20/10/24.
 //
+#include "imagesoa.hpp"
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <cmath>
+
+void scaleIntensity(ImageSOA_8bit& pixels, int currentMax, int newMax) {
+  // Usar tres bucles mejor para optimizacion de cache, (probablemente)
+  for (auto& redPixel : pixels.red) {
+    redPixel = static_cast<uint8_t>(std::round(redPixel * static_cast<double>(newMax) / currentMax));
+  }
+  for (auto& greenPixel : pixels.green) {
+    greenPixel = static_cast<uint8_t>(std::round(greenPixel * static_cast<double>(newMax) / currentMax));
+  }
+  for (auto& bluePixel : pixels.blue) {
+    bluePixel = static_cast<uint8_t>(std::round(bluePixel * static_cast<double>(newMax) / currentMax));
+  }
+}
+
+void scaleIntensity(ImageSOA_16bit& pixels, int currentMax, int newMax) {
+  for (auto& redPixel : pixels.red) {
+    redPixel = static_cast<uint16_t>(std::round(redPixel * static_cast<double>(newMax) / currentMax));
+  }
+  for (auto& greenPixel : pixels.green) {
+    greenPixel = static_cast<uint16_t>(std::round(greenPixel * static_cast<double>(newMax) / currentMax));
+  }
+  for (auto& bluePixel : pixels.blue) {
+    bluePixel = static_cast<uint16_t>(std::round(bluePixel * static_cast<double>(newMax) / currentMax));
+  }
+}
+
+ImageSOA_8bit loadImage(const std::string& filename, int width, int height, int maxColorValue) {
+  std::ifstream file(filename, std::ios::binary);
+  if (!file.is_open()) {
+    throw std::runtime_error("Could not open file " + filename);
+  }
+  // TODO
+};
--- imgsoa/imagesoa.hpp
+++ imgsoa/imagesoa.hpp
@@ -5,4 +5,45 @@
 #ifndef IMAGESOA_HPP
 #define IMAGESOA_HPP
 
-#endif //IMAGESOA_HPP
+#include <string>
+#include <vector>
+#include <cstdint>
+
+// Para imagenes con pixeles de 8 bits
+struct ImageSOA_8bit{
+  std::vector<uint8_t> red;    // Vector de valores rojos
+  std::vector<uint8_t> green;  // Vector de valores verdes
+  std::vector<uint8_t> blue;   // Vector de valores azules
+
+  // Inicializar los vectores a un tamaño determinado
+  ImageSOA_8bit(size_t vector_size) : red(vector_size), green(vector_size), blue(vector_size) {}
+};
+
+// Para imagenes con pixeles de 16 bits
+struct ImageSOA_16bit {
+  std::vector<uint16_t> red;    // Vector de valores rojos
+  std::vector<uint16_t> green;  // Vector de valores verdes
+  std::vector<uint16_t> blue;   // Vector de valores azules
+
+  // Inicializar los vectores a un tamaño determinado
+  ImageSOA_16bit(size_t vector_size) : red(vector_size), green(vector_size), blue(vector_size) {}
+};
+
+struct PPMMetadata {
+  std::string magicNumber;
+  int width;
+  int height;
+  int maxColorValue;
+};
+
+// Función para escalar la intensidad de cada píxel al nuevo valor máximo
+// NOTA: Se puede hacer con 8 y 16 bit con una funcion, pero queda mucho menos legible
+void scaleIntensity(ImageSOA_8bit& pixels, int currentMax, int newMax);
+
+void scaleIntensity(ImageSOA_16bit& pixels, int currentMax, int newMax);
+
+ImageSOA_8bit loadImage(const std::string& filename, int width, int height, int maxColorValue);
+
+
+#endif IMAGESOA_HPP
+
